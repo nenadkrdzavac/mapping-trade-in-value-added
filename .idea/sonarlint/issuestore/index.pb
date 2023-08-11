4f09402a22dafe1bd839a0ec4a3a41f59bef228f
nenadkrdzavac <nenad.krdzavac@gmail.com>
2023-08-11 09:14:07 +0200

    updated parsing country code

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+b
+2src/main/java/org/example/ParsingSPARQLSankey.java,d\8\d87d5f93df740417371223206e44a631b9b784b5
+]
+-src/main/java/org/example/ParsingNASACsv.java,4\e\4edc30a4b02d566f66aab6fbde37db20be336446
+]
+-src/main/java/org/example/ParsingTiVACsv.java,a\9\a9c8a0ced772a99e2d57d25f4a10e9fb1f45cbe5
+S
+#src/main/java/org/example/Test.java,a\d\ad60b8b6e75b7d80ea3441c372d5f5b965deca7c
+S
+#.idea/sonarlint/issuestore/index.pb,9\f\9fe84ebb15faf917b7def6236dba604453cc61e0
+]
+-.idea/sonarlint/securityhotspotstore/index.pb,8\8\88a9255124c95bdc913197c120a8d560edc59c8e
--- src/main/java/org/example/ParsingCountryTiva.java
+++ src/main/java/org/example/ParsingCountryTiva.java
@@ -3,16 +3,18 @@ package org.example;
 import java.io.*;
 import java.math.BigDecimal;
 import java.util.HashMap;
+import java.util.Map;
 
 public class ParsingCountryTiva {
 
 
     public static void main(String[] args) throws IOException {
 
-        HashMap<String,String> euCountriesMap = new HashMap<String, String>();
 
-        String csvFile = "C:\\Users\\KrdzavacN\\git\\mapping-trade-in-value-added\\src\\main\\resources\\exgr_bsci_2018_updated_1000_1.csv";
-        String csvFileUpdated = "C:\\Users\\KrdzavacN\\git\\mapping-trade-in-value-added\\src\\main\\resources\\exgr_bsci_2018_updated_1000_1_country.csv";
+
+        String csvFile = "U:\\tiva-graphs\\exgr_bsci_2018_updated\\input\\exgr_bsci_2018_updated_1000_1.csv";
+        String csvFileUpdated = "U:\\tiva-graphs\\exgr_bsci_2018_updated\\input\\exgr_bsci_2018_updated_1000_1_country.csv";
+
         getCountries(csvFile,csvFileUpdated);
 
     }
@@ -41,6 +43,8 @@ public class ParsingCountryTiva {
 
                 int lineNumber = 1;
 
+                HashMap<String, String> countryHashMap = new HashMap<String, String>();
+
                 while ((line = bufferReader.readLine()) != null) {
 
                     if(lineNumber>1) {
@@ -48,31 +52,33 @@ public class ParsingCountryTiva {
                         // use comma as separator
                         String[] linesArray = line.split(cvsSplitBy);
 
-                        String s = "";
+                        if(!countryHashMap.containsKey(linesArray[0])){
 
-                        s = s + linesArray[0] + "," + "http://data.coypu.org/country/"+linesArray[0] + ",";
+                            countryHashMap.put(linesArray[0], "http://data.coypu.org/country/"+linesArray[0]);
+                        }
 
-                        System.out.println(lineNumber+ "," + s);
+                        if(!countryHashMap.containsKey(linesArray[2])){
 
-                        writer.write(s);
-                        writer.write("\n");
+                            countryHashMap.put(linesArray[2], "http://data.coypu.org/country/"+linesArray[2]);
+                        }
 
-                        String p = "";
-
-                        p = p + linesArray[2] + "," + "http://data.coypu.org/country/"+linesArray[2] + ",";
-
-                        System.out.println(lineNumber+ "," + p);
-
-                        writer.write(p);
-                        writer.write("\n");
-
-
-
+                        System.out.println(linesArray[0] + "," + "http://data.coypu.org/country/"+linesArray[0] + ",");
+                        System.out.println(linesArray[2] + "," + "http://data.coypu.org/country/"+linesArray[2] + ",");
                     }
 
                     if(lineNumber==10)break;
 
                     lineNumber++;
+                }
+
+                System.out.println("Unique list of country codes:");
+
+                for(Map.Entry<String, String> m: countryHashMap.entrySet()) {
+
+                    System.out.println(m.getKey()+","+m.getValue()+",");
+
+                    writer.write(m.getKey()+","+m.getValue()+",");
+                    writer.write("\n");
 
                 }
 
